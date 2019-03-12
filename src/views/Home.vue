69eefa951196f559aa7508a06771ad2ac8dd8cfd
Antonija <antonija.pek@nosto.com>
2019-03-12 17:01:07 +0200

    Fetching method improved

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,39 +1,67 @@
 <template>
-  <div class="wrapper">
-    <h1>{{ $prismic.richTextAsPlain(data.site_title) }}</h1>
-  </div>
+	<div class="wrapper">
+		<h1>{{ $prismic.richTextAsPlain(homepage.data.site_title) }}</h1>
+		<!-- <pre>{{homepage}}</pre> -->
+		<ul>
+			<li v-for="(work, index) in works" :key="index">
+				{{ $prismic.richTextAsPlain(work.data.title) }}
+				<img :src="work.data.promo_image.url"/>
+			</li>
+		</ul>
+	</div>
 </template>
 
-<style lanf="scss" scoped>
-@import "../css/base.css";
+<style lang="scss" scoped>
+h1 {
+	text-align: center;
+	font-family: bio-sans, sans-serif;
+	font-size: 16px;
+	line-height: 1.6;
+}
+
+a {
+	font-family: bio-sans, sans-serif;
+	font-weight: 600;
+	font-style: italic;
+	line-height: 1.6;
+}
+
+img {
+	width: 100%;
+	height: auto;
+}
+
 .wrapper {
-  width: 960px;
-  margin: 10em auto;
+	width: 960px;
+	margin: 10em auto;
 }
 </style>
 
 
 <script>
 export default {
-  name: 'Home',
-  data() {
-    return {
-      data: {
-        site_title: "",
-        slot_1: "",
-        slot_2: ""
-      }
-    }
-  },
-  mounted() {
-    this.$nextTick(function () {
-      // Code that will run only after the
-      // entire view has been rendered
-      this.$prismic.client.getSingle("homepage").then((document) => {
-        this.data = document.data
-      });
-    })
-    
-  }
+	name: 'Home',
+	data() {
+		return {
+			homepage: {},
+			works: [],
+		}
+	},
+	mounted() {
+		this.$nextTick(function () {
+			this.$prismic.client.query(
+				this.$prismic.Predicates.any('document.type', ['work', 'homepage'])
+			).then((response) => {
+				this.home = response.results
+				response.results.map(item => {
+					if (item.type === "work") {
+						this.works.push(item)
+					} else if (item.type === "homepage") {
+						this.homepage = item
+					}
+				})	
+			});
+		})
+	}
 }
 </script>
