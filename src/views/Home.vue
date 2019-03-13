9f416ebf67cb957ad111a3391910a50417c84b50
Antonija <antonija.pek@nosto.com>
2019-03-13 12:21:16 +0200

    Base for CSS grid and separate SCSS file for share styles

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,39 +1,32 @@
 <template>
 	<div class="wrapper">
 		<h1>{{ $prismic.richTextAsPlain(homepage.data.site_title) }}</h1>
-		<!-- <pre>{{homepage}}</pre> -->
-		<ul>
-			<li v-for="(work, index) in works" :key="index">
-				{{ $prismic.richTextAsPlain(work.data.title) }}
-				<img :src="work.data.promo_image.url"/>
-			</li>
-		</ul>
+		<div class="projects">
+			<div class="project" v-for="(work, index) in works" :key="index">
+			<!-- {{ $prismic.richTextAsPlain(work.data.title) }} -->
+			<img :src="work.data.promo_image.url"/>
+		</div>
+		</div>
 	</div>
 </template>
 
 <style lang="scss" scoped>
-h1 {
-	text-align: center;
-	font-family: bio-sans, sans-serif;
-	font-size: 16px;
-	line-height: 1.6;
-}
-
-a {
-	font-family: bio-sans, sans-serif;
-	font-weight: 600;
-	font-style: italic;
-	line-height: 1.6;
-}
-
-img {
-	width: 100%;
-	height: auto;
-}
+@import "../assets/scss/main.scss";
 
 .wrapper {
-	width: 960px;
-	margin: 10em auto;
+  width: 90%;
+  margin: 10em auto;
+
+  .projects {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-column-gap: $space-base;
+    grid-row-gap: $space-base;
+
+    @include md {
+      grid-template-columns: 1fr 1fr;
+    }
+  }
 }
 </style>
 
