349a8b13baf6ca0c04cb1b59471d1a62b8f6b232
Antonija <antonija.pek@nosto.com>
2019-05-07 16:45:59 +0300

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,21 +4,12 @@
     <div class="projects">
       <div class="project" v-for="(work, index) in works" :key="index">
         <router-link :to="`/${work.uid}`">
-          <div class="rectangle">
-            <img :src="work.data.promo_image.url">
-          </div>
-          <h2>{{ $prismic.richTextAsPlain(work.data.title) }}</h2>
-          <h3>{{ $prismic.richTextAsPlain(work.data.description) }}</h3>
+          <h2>{{ $prismic.richTextAsPlain(work.data.title) }} — {{ $prismic.richTextAsPlain(work.data.description) }}</h2>
         </router-link>
       </div>
     </div>
     <div class="more">
       <prismic-rich-text v-if="homepage" :field="homepage.data.slot_1"/>
-      <div>
-        <h3>Latest From Blog</h3>
-        <h4>Make a design system with Figma</h4>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, quos.</p>
-      </div>
     </div>
   </div>
 </template>
@@ -31,109 +22,49 @@
   margin: 4em auto;
 
   @include md {
-    // width: 70%;
     width: 960px;
     margin: 10em auto;
   }
 
-  @include xl {
-    // width: 40%;
-  }
-
   /deep/ p {
-    font-size: 16px;
+    font-size: 18px;
     margin-bottom: $space-medium;
-    line-height: 1.5;
+    line-height: 1.8;
   }
 
   /deep/ h1 {
-    font-size: 22px;
+    font-size: 18px;
     margin-bottom: $space-medium;
-    line-height: 1.5;
-
-    @include md {
-      font-size: 32px;
-    }
+    line-height: 1.8;
   }
 
   .projects {
-    display: grid;
-    grid-template-columns: 1fr;
-    grid-column-gap: $space-base;
-    grid-row-gap: $space-base;
     margin-bottom: $space-base * 8;
     margin-top: $space-base * 8;
 
     @include md {
       grid-template-columns: 1fr 1fr;
-      margin-bottom: $space-base * 16;
-      margin-top: $space-base * 16;
+      margin-bottom: $space-base * 8;
+      margin-top: $space-base * 8;
     }
 
     .project {
-      position: relative;
-      // overflow: hidden;
-
-      a {
-        &:hover {
-          .rectangle {
-            // background-color: #9d72c9;
-          }
-        }
-      }
-
-      /deep/ p {
-        font-family: Spectral;
-      }
-
-      img {
-        transition: transform 300ms ease-out;
-
-        &:hover {
-          transform: translateY(-10px);
-        }
-      }
-
       h2 {
-        font-size: 24px;
+        font-size: 18px;
         margin-top: $space-medium;
         margin-bottom: $space-base;
-      }
-
-      h3 {
-        font-size: 16px;
-        font-family: Spectral;
-        font-weight: 400;
-        letter-spacing: -0.5px;
-      }
-
-      .rectangle {
-        background-color: #c299ea;
-        border-radius: 5px;
       }
     }
   }
 }
 
 .more {
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-column-gap: $space-large;
-  grid-row-gap: $space-base;
-
   /deep/ h3 {
     color: #888888;
-    font-size: 16px;
+    font-size: 18px;
     text-transform: uppercase;
     font-weight: 500;
     margin-bottom: $space-medium;
-  }
-
-  /deep/ h4 {
-    font-family: Spectral;
-    font-size: 19px;
-    font-weight: 500;
-    margin-bottom: $space-base;
   }
 }
 </style>
